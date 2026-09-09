9ea4e97738fd3054a426f84a649f73e785d6a202
Andrew Franks <andrewmfranks1995@yahoo.com>
2026-09-09 12:54:35 -0400

    Adds LinearSearchDemo.java

---
+++ src/main/java/com/amfranks/datastructuresinjava/ch02_searching_and_analysis/LinearSearchDemo.java
@@ -0,0 +1,37 @@
+package com.amfranks.datastructuresinjava.ch02_searching_and_analysis;
+
+import java.util.Scanner;
+
+public class LinearSearchDemo {
+
+    static int linearSearch(int[] numbers, int key) {
+        for (int i = 0; i < numbers.length; i++) {
+            if (numbers[i] == key) {
+                return i;
+            }
+        }
+
+        return -1; // key not found
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {2, 4, 7, 10, 11, 32, 45, 87};
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("NUMBERS: ");
+        for (int i = 0; i < numbers.length; i++) {
+            System.out.println(numbers[i]);
+        }
+
+        System.out.println("\nEnter a value: ");
+        int key = scanner.nextInt();
+
+        int keyIndex = linearSearch(numbers, key);
+
+        if (keyIndex == -1) {
+            System.out.println("\n" + key + " was not found.");
+        } else {
+            System.out.println("\nFound " + key + " at index " + keyIndex + ".");
+        }
+    }
+}
